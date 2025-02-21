d8c5f3eb0517a7f5aa2f9b1051757f766b2af5fe
Sreeraj <154326486+s-r-e-e-r-a-j@users.noreply.github.com>
2025-02-21 21:57:00 +0530

    Create code.ino

---
+++ MotionDetectionLight/code.ino
@@ -0,0 +1,30 @@
+
+int sensorpin=7;
+int relaypin=5;
+
+void setup() {
+  
+Serial.begin(9600);
+pinMode(sensorpin, INPUT);
+pinMode(relaypin, OUTPUT);
+
+}
+
+void loop() {
+  
+  int pirstate=digitalRead(sensorpin);
+
+ Serial.println(pirstate);
+
+  if(pirstate == HIGH){
+
+    //turn on the bulb
+    digitalWrite(relaypin, LOW);
+
+  }else{
+
+    //turn off the bulb
+    digitalWrite(relaypin, HIGH);
+  }
+
+}
